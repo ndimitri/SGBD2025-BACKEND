d2a746024438d55c0fe5107caf06478e1182e4ac
Dimitriadis Nicolas <dimitriadisnicolas@gmail.com>
2025-02-15 15:21:48 +0100

    git add : add BaseEntity entity constructor

--- src/main/java/be/nike/projet_sgbd_2025/dl/entities/BaseEntity.java
+++ src/main/java/be/nike/projet_sgbd_2025/dl/entities/BaseEntity.java
@@ -7,7 +7,9 @@ import jakarta.persistence.MappedSuperclass;
 import java.time.LocalDateTime;
 import java.util.UUID;
 import lombok.EqualsAndHashCode;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 import lombok.ToString;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
@@ -17,9 +19,11 @@ import org.hibernate.annotations.UpdateTimestamp;
 @EqualsAndHashCode(of = {"id"})
 @ToString
 @MappedSuperclass
+@Getter
 public abstract class BaseEntity {
 
   @Id
+  @Setter
   private UUID id;
 
   @CreationTimestamp
@@ -27,5 +31,9 @@ public abstract class BaseEntity {
 
   @UpdateTimestamp
   private LocalDateTime updateAt;
+
+  public BaseEntity(UUID id) {
+    this.id = id;
+  }
 
 }
