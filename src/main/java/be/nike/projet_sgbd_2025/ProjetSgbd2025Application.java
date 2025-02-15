14695b326ad6b70ecfcade1ccb458921f305d390
Dimitriadis Nicolas <dimitriadisnicolas@gmail.com>
2025-02-15 14:58:57 +0100

    git init

---
+++ src/main/java/be/nike/projet_sgbd_2025/ProjetSgbd2025Application.java
@@ -0,0 +1,13 @@
+package be.nike.projet_sgbd_2025;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetSgbd2025Application {
+
+  public static void main(String[] args) {
+    SpringApplication.run(ProjetSgbd2025Application.class, args);
+  }
+
+}
---
+++ src/test/java/be/nike/projet_sgbd_2025/ProjetSgbd2025ApplicationTests.java
@@ -0,0 +1,13 @@
+package be.nike.projet_sgbd_2025;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetSgbd2025ApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
